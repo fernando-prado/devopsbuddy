d3c72c4ec3e07609aba919e96a266a3778981964
Fernando Prado <fprado@gmail.com>
2017-02-27 19:27:54 -0300

    Added i18n feature

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,29 @@
 package com.devopsbuddy;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.devopsbuddy.web.i18n.I18NService;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	I18NService i18NService;
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected strings don't match", expectedResult, actual);
+		
 	}
 
 }
